3018aee7c72467b3e47a22df6202e46b98c91efe
Oliver Zott <oliver.zott@agindo.at>
2026-03-03 12:36:28 +0100

    chore: Remove unused platforms

--- workout-app/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace workout_app;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
