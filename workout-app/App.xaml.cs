894cd195e206d4183ea41cbd24756590d16da982
Oliver Zott <zott_oliver@web.de>
2026-03-21 17:09:15 +0100

    fix: remove fire-and-forget database seeding

--- workout-app/App.xaml.cs
+++ workout-app/App.xaml.cs
@@ -2,22 +2,26 @@ namespace workout_app;
 
 public partial class App : Application
 {
+    private readonly DatabaseService _database;
+
     public App(DatabaseService database)
     {
         InitializeComponent();
-
-        // Run DB seed on startup
-        Task.Run(async () =>
-        {
-            await database.InitializeAsync();
-        });
+        _database = database;
 
         // Force Light Mode - Prevent automatic dark mode switching
+        // TODO: implement dark mode support
         UserAppTheme = AppTheme.Light;
     }
 
     protected override Window CreateWindow(IActivationState? activationState)
     {
         return new Window(new AppShell());
+    }
+
+    protected override async void OnStart()
+    {
+        base.OnStart();
+        await _database.InitializeAsync();
     }
 }
